2aeff662b9d9a26b5d50a38090209d85616ba814
Max Gurela <maxpowa1@gmail.com>
2014-07-12 11:24:45 -0700

    Remove Mojang AccountsClient code - it wasn't being used.

--- src/main/java/com/maxpowa/chime/util/Utils.java
+++ src/main/java/com/maxpowa/chime/util/Utils.java
@@ -1,51 +1,13 @@
 package com.maxpowa.chime.util;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
-import net.minecraft.util.Session;
 
 import org.apache.logging.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.mojang.api.http.BasicHttpClient;
-import com.mojang.api.http.HttpBody;
-import com.mojang.api.http.HttpClient;
-import com.mojang.api.http.HttpHeader;
 
 public class Utils {
-	
-    private static HttpClient client;
     
     public static HashMap<String, User> users = new HashMap<String, User>();
 	
 	public static Logger log = null;
-    
-    static {
-    	client = BasicHttpClient.getInstance();
-    }
-    
-	public static boolean isValid(Session session) {
-		try {
-            HttpBody body = new HttpBody("{\"accessToken\": \"" + session.getToken() + "\"}");
-            List<HttpHeader> headers = new ArrayList<HttpHeader>();
-            headers.add(new HttpHeader("Content-Type", "application/json"));
-            
-            JSONObject result = post(new URL("https://api.mojang.com/validate"), body, headers);
-            Utils.log.info("Got validation response => "+result.toString());
-            return result.names().length() > 0;
-        } catch (Exception e) {
-	        return false;
-        }
-	}
-	
-	public static JSONObject post(URL url, HttpBody body, List<HttpHeader> headers) throws IOException, JSONException {
-        String response = client.post(url, body, headers);
-        return new JSONObject(response);
-    }
 	
 }
