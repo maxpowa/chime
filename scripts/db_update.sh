615216bfb6a6c7a899ef87d786571e4a8272eb2b
Max Gurela <maxpowa1@gmail.com>
2014-07-29 00:01:15 -0700

    Can confirm, 10/10 nub

--- scripts/db_update.sh
+++ scripts/db_update.sh
@@ -2,3 +2,4 @@
 JAVA_VERSION=`java -version 2>&1 | grep "java version" | awk '{print $3}' | sed -e "s/\"//g"`
 TEMP_JSON=`printf '{"%s":{"JAVA_VERSION":"%s","BRANCH":"%s","JOB_ID":"%s"}}\n' "$COMMIT" "$JAVA_VERSION" "$BRANCH" "$JOB_ID"`
 curl -X PUT -d $TEMP_JSON https://irc-bot-backend.firebaseio.com/builds.json?auth=cufRWsNn5Xos90zvdJE8qHMp2kCiXwZ6Xw5a2n6p 
+echo $TEMP_JSON
