5fd1689e03fbea2f3c10e1400f3105e5c8b3230f
Max Gurela <maxpowa1@gmail.com>
2014-07-29 00:09:19 -0700

    Change db format

--- scripts/db_update.sh
+++ scripts/db_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 JAVA_VERSION=`java -version 2>&1 | grep "java version" | awk '{print $3}' | sed -e "s/\"//g"`
-TEMP_JSON=`printf '{"%s":{"JAVA_VERSION":"%s","BRANCH":"%s","JOB_ID":"%s"}}\n' "$COMMIT" "$JAVA_VERSION" "$BRANCH" "$JOB_ID"`
+TEMP_JSON=`printf '{"%s":{"COMMIT":"%s","JAVA_VERSION":"%s","BRANCH":"%s","JOB_ID":"%s"}}\n' "$BUILD_NUMBER" "$COMMIT" "$JAVA_VERSION" "$BRANCH" "$JOB_ID"`
 curl -X PUT -d $TEMP_JSON https://irc-bot-backend.firebaseio.com/builds.json?auth=cufRWsNn5Xos90zvdJE8qHMp2kCiXwZ6Xw5a2n6p 
 echo $TEMP_JSON
