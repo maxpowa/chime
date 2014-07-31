89d7d9e5be34adb8a85e8b4fdbbf905a84fb848b
Max Gurela <maxpowa1@gmail.com>
2014-07-31 11:01:21 -0700

    Nope, just bad at everything

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,19 +1,19 @@
 #!/bin/bash 
 MC_VERSION=`echo "$FORGEVERSION" | awk -F'-' '{print $1}'`
-FILE_NAME="Chime-$MAJOR_MINOR.$BUILD_NUMBER.jar"
-FILE_LOCATION="build/libs/$FILE_NAME"
-JSON='{
+FILE_NAME=`echo "Chime-$MAJOR_MINOR.$BUILD_NUMBER.jar"`
+FILE_LOCATION=`echo "build/libs/$FILE_NAME"`
+JSON=`printf '{
   "version": {
-    "name": "$MAJOR_MINOR.$BUILD_NUMBER",
-    "minecraft": "$MC_VERSION",
-    "changelog": "http://github.com/maxpowa/chime/commits/$COMMIT"
+    "name": "%s.%s",
+    "minecraft": "%s",
+    "changelog": "http://github.com/maxpowa/chime/commits/%s"
   },
-  "filename": "$FILE_NAME"
-}'
+  "filename": "%s"
+}' "$MAJOR_MINOR" "$BUILD_NUMBER" "$MC_VERSION" "$COMMIT" "$FILE_NAME"`
 
 echo "Ready to push file to mods.io"
 echo "$JSON"
 
 if [ "$PUSH" = true ] ; then
-    curl -H 'X-API-Key: $MODS_IO_APIKEY' -X POST -H 'Accept: application/json' -F body='$JSON' -F file='@$FILE_LOCATION' https://mods.io/mods/1086/versions/create.json
+    curl -H 'X-API-Key: ${MODS_IO_APIKEY}' -X POST -H 'Accept: application/json' -F body='$JSON' -F file='@${FILE_LOCATION}' https://mods.io/mods/1086/versions/create.json
 fi
