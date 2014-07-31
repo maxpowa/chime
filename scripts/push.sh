da3ea7bf8b2d025bc55f56abc55ee97a6e31ed25
Max Gurela <maxpowa1@gmail.com>
2014-07-31 12:05:33 -0700

    I'm going crazy, quotations are really annoying.

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,7 +2,7 @@
 MC_VERSION=`echo "$FORGEVERSION" | awk -F'-' '{print $1}'`
 FILE_NAME=`echo "Chime-$MAJOR_MINOR.$BUILD_NUMBER.jar"`
 FILE_LOCATION=`echo "./build/libs/$FILE_NAME"`
-JSON=`printf '{"version": { "name": "%s.%s", "minecraft": "%s", "changelog": "[Commit %s](http://github.com/maxpowa/chime/commits/%s)", "tag": "%s" }, "filename": "%s" }' "$MAJOR_MINOR" "$BUILD_NUMBER" "$MC_VERSION" "$COMMIT" "$COMMIT" "$TAG" $FILE_NAME"`
+JSON=`printf '{"version": { "name": "%s.%s", "minecraft": "%s", "changelog": "[Commit %s](http://github.com/maxpowa/chime/commits/%s)", "tag": "%s" }, "filename": "%s" }' "$MAJOR_MINOR" "$BUILD_NUMBER" "$MC_VERSION" "$COMMIT" "$COMMIT" "$TAG" "$FILE_NAME"`
 
 echo "Ready to push file to mods.io"
 echo "$JSON"
