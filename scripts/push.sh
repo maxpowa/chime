d6e7c1013df1f4658396a26e69ea3bbccd775495
Max Gurela <maxpowa1@gmail.com>
2014-07-31 11:45:27 -0700

    I'm really bad at JSON too.

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,14 +2,7 @@
 MC_VERSION=`echo "$FORGEVERSION" | awk -F'-' '{print $1}'`
 FILE_NAME=`echo "Chime-$MAJOR_MINOR.$BUILD_NUMBER.jar"`
 FILE_LOCATION=`echo "./build/libs/$FILE_NAME"`
-JSON=`printf '\{
-  \"version\": \{
-    \"name\": \"%s.%s\",
-    \"minecraft\": \"%s\",
-    \"changelog\": \"http://github.com/maxpowa/chime/commits/%s\"
-  \},
-  \"filename\": \"%s\"
-\}' "$MAJOR_MINOR" "$BUILD_NUMBER" "$MC_VERSION" "$COMMIT" "$FILE_NAME"`
+JSON=`printf '{"version": { "name": "%s.%s", "minecraft": "%s", "changelog": "http://github.com/maxpowa/chime/commits/%s" }, "filename": "%s" }' "$MAJOR_MINOR" "$BUILD_NUMBER" "$MC_VERSION" "$COMMIT" "$FILE_NAME"`
 
 echo "Ready to push file to mods.io"
 echo "$JSON"
