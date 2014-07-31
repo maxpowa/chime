32ede804d2ae3539cd22e5c6d6108fc3e02fc9ea
Max Gurela <maxpowa1@gmail.com>
2014-07-31 10:47:23 -0700

    Testing push straight to mods.io

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,2 +1,19 @@
 #!/bin/bash 
-find .
+MC_VERSION=`echo "$FORGEVERSION" | awk -F'-' '{print $1}'`
+FILE_NAME="Chime-$MAJOR_MINOR.$BUILD_NUMBER.jar"
+FILE_LOCATION="build/libs/$FILE_NAME"
+JSON='{
+  "version": {
+    "name": "$MAJOR_MINOR.$BUILD_NUMBER",
+    "minecraft": "$MC_VERSION",
+    "changelog": "http://github.com/maxpowa/chime/commits/$COMMIT"
+  },
+  "filename": "$FILE_NAME"
+}'
+
+echo "Ready to push file to mods.io"
+echo "$JSON"
+
+if [ "$PUSH" = true ] ; then
+    curl -H 'X-API-Key: $MODS_IO_APIKEY' -X POST -H 'Accept: application/json' -F body='$JSON' -F file='@$FILE_LOCATION' https://mods.io/mods/1086/versions/create.json
+fi
