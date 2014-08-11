048d2cf9498393506013f5e51aa2c1fbe55b6dd0
maxgurela <maxgurela@10.10.1.132>
2014-08-11 10:01:32 -0700

    Push to Curse as well

---
+++ scripts/curse_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+FILE_NAME=`echo "Chime-$MAJOR_MINOR.$BUILD_NUMBER.jar"`
+FILE_LOCATION=`echo "./build/libs/$FILE_NAME"`
+JSON=`printf '{"changelog": "See http://github.com/maxpowa/chime/commit/%s for full change details.", "gameVersions":%s, "releaseType": "%s"}' "$COMMIT" "$CURSE_VERSION_IDS" "$TAG"`
+
+echo "Ready to push file to curse"
+echo "$JSON"
+
+if [ "$PULL_REQUEST" = "None" ] ; then
+    if [ "$PUSH" = true ] ; then
+       
+        curl -i -H "X-Api-Token: $CURSE_API_KEY" \
+        -X POST \
+        -F metadata="$JSON" \
+        -F file="@$FILE_LOCATION" \
+        http://minecraft.curseforge.com/api/projects/223265/upload-file
+
+    fi
+fi
