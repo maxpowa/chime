a92d89dfdce43906f232401dd7d4023e6eb8fc7f
maxgurela <maxgurela@10.10.1.132>
2014-08-12 11:15:35 -0700

    Still invalid...

--- scripts/curse_push.sh
+++ scripts/curse_push.sh
@@ -15,7 +15,7 @@ if [ "$PULL_REQUEST" = "None" ] ; then
         -F file="@$FILE_LOCATION" \
         http://minecraft.curseforge.com/api/projects/223265/upload-file)
 
-        TEMP_JSON=`printf '{"curse":"%s", "type":"%s", "public":"%s", "version":"%s"}' "$RESPONSE" "$TAG" "$PUSH" "$MAJOR_MINOR.$BUILD_NUMBER"` 
+        TEMP_JSON=`printf '{"curse":"%s", "type":\"%s\", "public":"%s", "version":\"%s\"}' "$RESPONSE" "$TAG" "$PUSH" "$MAJOR_MINOR.$BUILD_NUMBER"` 
         URL=`printf 'https://irc-bot-backend.firebaseio.com/releases/%s.json?auth=cufRWsNn5Xos90zvdJE8qHMp2kCiXwZ6Xw5a2n6p' "$BUILD_NUMBER"` 
         curl -X PUT -d $TEMP_JSON $URL 
         echo $TEMP_JSON
